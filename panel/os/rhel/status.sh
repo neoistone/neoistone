20aad8f5c346139386550b671a681fc50ab8b361
Neoistone india <59179052+neoistone@users.noreply.github.com>
2020-09-17 02:01:11 +0530

    Update status.sh

--- panel/os/rhel/status.sh
+++ panel/os/rhel/status.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+#neoistone.com
 status=`subscription-manager list | egrep -i 'Status:         Subscribed'`
 if [ "${status}" == "Status:         Subscribed" ]; then
    echo "Subscribed"
 else 
 echo "Not Subscribe"
-if
+fi
